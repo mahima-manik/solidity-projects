75c8729dcb4b6081432365be4290b8469a887ae0
Mahima Manik <mahimamanik.22@gmail.com>
2022-03-11 16:34:47 +0530

    Changing Birthday struct and adding more functions

--- contracts/Occasion.sol
+++ contracts/Occasion.sol
@@ -6,18 +6,34 @@ import "@quant-finance/solidity-datetime/contracts/DateTime.sol";
 contract Occasion {
     struct Birthday {
         uint date;
+        uint month;
+        uint year;
     }
 
     mapping (address => Birthday) birthdays;
-    function set(uint _my_birthday) external {
-        birthdays[msg.sender] = Birthday({date: _my_birthday});
+    function set(uint _date, uint _month, uint _year) external {
+        bool valid = DateTime.isValidDate(_year, _month, _date);
+        require (valid == true, "Not a valid date");
+        birthdays[msg.sender] = Birthday({date: _date, month: _month, year: _year});
     }
 
-    function get() external view returns (uint) {
-        return birthdays[msg.sender].date;
+    function checkValid(uint _date, uint _month, uint _year) external pure returns (bool) {
+        bool valid = DateTime.isValidDate(_year, _month, _date);
+        return valid;
     }
 
-    function getMonth() external view returns (uint)    {
-        return DateTime.getMonth(birthdays[msg.sender].date);
+    function get() external view returns (uint, uint, uint) {
+        return (birthdays[msg.sender].date, birthdays[msg.sender].month, birthdays[msg.sender].year);
+    }
+
+    function is_my_birthday() external view returns (bool) {
+        uint time_now = block.timestamp;
+        uint month_now = DateTime.getMonth(time_now);
+        uint day_now = DateTime.getDay(time_now);
+        
+        if (month_now != birthdays[msg.sender].month ||
+            day_now != birthdays[msg.sender].date) return false;
+        
+        return true;
     }
 }
--- test/Occasion.test.js
+++ test/Occasion.test.js
@@ -1,15 +1,36 @@
 const Occasion = artifacts.require("Occasion");
 
 contract("Occasion", (accounts) => {
+    let my_birthday, my_birthday_date, my_birthday_month, my_birthday_year
     before(async () => {
         contractInstance = await Occasion.deployed();
+        my_birthday = new Date (Date.now());
+        my_birthday_date = my_birthday.getDate()
+        my_birthday_month = my_birthday.getMonth()+1
+        my_birthday_year = my_birthday.getFullYear()
     });
 
-    it ('Set and check birthday', async () => {
-        const my_birthday = Date.now();
-        await contractInstance.set(my_birthday);
-        const get_birthday = await contractInstance.get();
-        assert.equal(my_birthday, get_birthday, "Birthday not set correctly")
+    it ('Check validity', async () => {
+        const valid = await contractInstance.checkValid(my_birthday_date, my_birthday_month, my_birthday_year);
+        console.log(valid)
+    });
+
+    it ('Set and check birthday', async () => {        
+        await contractInstance.set(my_birthday_date, my_birthday_month, my_birthday_year);
+        const birthday = await contractInstance.get();
+        
+        birthday_date = birthday[0].toNumber()
+        birthday_month = birthday[1].toNumber()
+        birthday_year = birthday[2].toNumber()
+        
+        assert.equal(birthday_date, my_birthday_date, "Birthday date not set correctly")
+        assert.equal(birthday_month, my_birthday_month, "Birthday month not set correctly")
+        assert.equal(birthday_year, my_birthday_year, "Birthday year not set correctly")
+    });
+
+    it ('Check if today is my birthday', async () => {        
+        const is_my_birthday = await contractInstance.is_my_birthday();
+        assert.equal(is_my_birthday, true, "Birthday should be today")
     });
 
 });
