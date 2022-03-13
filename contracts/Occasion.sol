2e29fe1ffc9a7a9fbec7a6f86a4b63d8e02baf32
Mahima Manik <mahimamanik.22@gmail.com>
2022-03-13 15:01:06 +0530

    Bestow and get all functions added

--- contracts/Occasion.sol
+++ contracts/Occasion.sol
@@ -4,81 +4,109 @@ pragma solidity 0.8.11;
 import "@quant-finance/solidity-datetime/contracts/DateTime.sol";
 
 contract Occasion {
+
+    /* Struct for storing Birthday and gifts received */
     struct Birthday {
         uint date;
         uint month;
         uint year;
-        mapping (address => uint) gifts;
+        uint total;
+        mapping (address => uint) gifts; // To be used later
     }
 
+    /* Emitted when gift is sent to an address */
     event GiftSent(address sender, address receiver, uint amount);
 
-    uint total = 0;
+    /* Emitted when an address withdraws the birthday fund */
+    event HappyBirthday(address receiver, uint amount);
+
+    uint totalBirthdays = 0;
     mapping (uint => address) indexes;
     mapping (address => Birthday) birthdays;
     
+    /* Sets the Birthday for msg.sender */
     function set(uint _date, uint _month, uint _year) external {
         bool valid = checkValid(_date, _month, _year);
         require (valid == true, "Not a valid date");
-        indexes[++total] = payable(msg.sender);
+
+        indexes[totalBirthdays++] = msg.sender;
         Birthday storage bday = birthdays[msg.sender];
         bday.date = _date;
         bday.month = _month;
         bday.year = _year;
     }
-
+    
+    /* Check if the date is valid */
     function checkValid(uint _date, uint _month, uint _year) public pure returns (bool) {
         bool valid = DateTime.isValidDate(_year, _month, _date);
         return valid;
     }
 
+    /* Get the birthday of msg.sender */
     function get() external view returns (uint, uint, uint) {
-        return (birthdays[msg.sender].date, birthdays[msg.sender].month, birthdays[msg.sender].year);
+        Birthday storage birthday = birthdays[msg.sender];
+        bool valid = checkValid(birthday.date, birthday.month, birthday.year);
+        require (valid == true, "Birthday does not exist");
+        return (birthday.date, birthday.month, birthday.year);
     }
 
+    /* Check if today is the birthday of msg.sender */
     function is_my_birthday() public view returns (bool) {
+        Birthday storage birthday = birthdays[msg.sender];
+        bool valid = checkValid(birthday.date, birthday.month, birthday.year);
+        require (valid == true, "Birthday does not exist");
+        
         uint time_now = block.timestamp;
         uint month_now = DateTime.getMonth(time_now);
         uint day_now = DateTime.getDay(time_now);
         
-        if (month_now != birthdays[msg.sender].month ||
-            day_now != birthdays[msg.sender].date) return false;
-        
+        if (month_now != birthday.month || day_now != birthday.date) return false;
         return true;
     }
 
+    /* Send gift to receiver */
     function send_gift(address receiver) external payable {
         Birthday storage birthday = birthdays[receiver];
         bool valid = checkValid(birthday.date, birthday.month, birthday.year);
         require (valid == true, "Receiver's birthday does not exist");
 
         birthday.gifts[msg.sender] += msg.value;
-        emit GiftSent(msg.sender, receiver, msg.value);
+        birthday.total += msg.value;
+        emit GiftSent(msg.sender, receiver, birthday.total);
     }
 
-    // function bestow_gift() external payable {
-    //     bool valid = is_my_birthday();
-    //     require (valid == true, "Cannot withdraw funds outside your birthday");
-    //     Birthday storage birthday = birthdays[msg.sender];
-    //     bool valid = checkValid(birthday.date, birthday.month, birthday.year);
-    //     require (valid == true, "Receiver's birthday does not exist");
+    /* Withdraws the total gift of msg.sender from the contract */
+    function bestow_gift() external payable {
+        bool valid = is_my_birthday();
+        require (valid == true, "Cannot withdraw funds outside your birthday");
+        Birthday storage birthday = birthdays[msg.sender];
 
-    //     birthday.gifts[msg.sender] += msg.value;
-    //     emit GiftSent(msg.sender, receiver, msg.value);
-    // }
+        payable(msg.sender).transfer(birthday.total);
+        
+        emit HappyBirthday(msg.sender, birthday.total);
+        birthday.total = 0;
+    }
 
-    // function get_total_gift() {
-    //     birthdays[msg.sender].gifts;
-    // }
+    /* Returns the total gift received until now for msg.sender */
+    function get_total_gift() public view returns (uint) {
+        Birthday storage birthday = birthdays[msg.sender];
+        bool valid = checkValid(birthday.date, birthday.month, birthday.year);
+        require (valid == true, "Birthday does not exist");
+        return birthdays[msg.sender].total;
+    }
 
-    // function get_all_birthdays() external view returns (address[], uint[], uint[]) {
-    //     address[] memory adrs;
-    //     address[] memory dates;
-    //     address[] memory months;
-    //     for (uint i = 0; i < total; i++)    {
-    //         address adr = indexes[i];
-    //         dates.Birthday[adr].date;
-    //     }
-    // }
-
+    /* Returns list of all addresses and birthdays stored on the contract */
+    function get_all_birthdays() external view returns (address[] memory, uint[] memory, uint[] memory) {
+        address[] memory adrs = new address[](totalBirthdays);
+        uint[] memory dates = new uint[](totalBirthdays);
+        uint[] memory months = new uint[](totalBirthdays);
+        
+        for (uint i = 0; i < totalBirthdays; i++)    {
+            address adr = indexes[i];
+            adrs[i] = adr;
+            dates[i] = birthdays[adr].date;
+            months[i] = birthdays[adr].month;
+        }
+        return (adrs, dates, months);
+    }
 }
---
+++ occasion.js
@@ -0,0 +1,61 @@
+const OccasionContract = artifacts.require("Occasion");
+
+async function getBalanceInEther(account)    {
+    balance = parseFloat(await web3.eth.getBalance(account));
+    return balance;
+}
+
+module.exports = async function(callback) {
+
+    let accounts = await web3.eth.getAccounts();
+
+    let contractInstance = await OccasionContract.deployed();
+    console.log("Contract instance fetched: ", contractInstance.address);
+    
+    let my_birthday, my_birthday_date, my_birthday_month, my_birthday_year
+    my_birthday = new Date (Date.now());
+    my_birthday_date = my_birthday.getDate()
+    my_birthday_month = my_birthday.getMonth()+1
+    my_birthday_year = my_birthday.getFullYear()
+
+    // Check if birthday is a valid date
+    const valid = await contractInstance.checkValid(my_birthday_date, my_birthday_month, my_birthday_year);
+    console.log("Is valid birthday: ", valid)
+
+    // Set birthday for few addresses
+    console.log("Setting Birthday to: ", my_birthday_date, my_birthday_month, my_birthday_year)
+    await contractInstance.set(my_birthday_date, my_birthday_month, my_birthday_year, {from: accounts[0]});
+    await contractInstance.set(my_birthday_date+3, my_birthday_month+2, my_birthday_year, {from: accounts[2]});
+    await contractInstance.set(my_birthday_date+10, my_birthday_month+5, my_birthday_year, {from: accounts[3]});
+    const birthday = await contractInstance.get();
+    
+    birthday_date = birthday[0].toNumber()
+    birthday_month = birthday[1].toNumber()
+    birthday_year = birthday[2].toNumber()
+    
+    console.log(birthday_date, my_birthday_date)
+    console.log(birthday_month, my_birthday_month)
+    console.log(birthday_year, my_birthday_year)
+
+    // Send gift and check if contract fund has increased
+    const giftAmount = web3.utils.toWei('1')
+    let result = await contractInstance.send_gift(accounts[0], {from: accounts[1], value: giftAmount});
+    let contractFund = await web3.eth.getBalance(contractInstance.address)
+
+    console.log("Contract fund is: ", contractFund)
+
+    // Get total gift of an address
+    result = await contractInstance.get_total_gift({from: accounts[0]});
+    console.log("Total gift is: ", result.toString());
+
+    // List all addresses and birthdays
+    result = await contractInstance.bestow_gift({from: accounts[0]});
+    contractFund = await web3.eth.getBalance(contractInstance.address)
+    console.log("Contract fund is now: ", contractFund)
+
+    result = await contractInstance.get_all_birthdays()
+    console.log(result[0])
+    console.log(result[1])
+    console.log(result[2])
+    callback();
+}
